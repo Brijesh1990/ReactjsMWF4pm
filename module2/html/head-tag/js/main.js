841da34f61dc90529cb330bebfdcbb17b55a6093
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-01-19 17:33:00 +0530

    19-01-2024 all data uploaded successfully

---
+++ module2/html/head-tag/js/main.js
@@ -0,0 +1,11 @@
+function test()
+{
+
+const btn=document.querySelector(".btn");
+btn.style.width="100px";
+btn.style.height="auto";
+btn.style.border="none";
+btn.style.backgroundColor="red";
+btn.style.padding="8px";
+
+}
