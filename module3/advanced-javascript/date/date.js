2ce7e9035b8e284dfaf5c571ecd9837b83262c5d
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-03-06 17:41:36 +0530

    06-03-2024 data uploaded

---
+++ module3/advanced-javascript/date/date.js
@@ -0,0 +1,25 @@
+// GMT+5:30 unix time stamp
+//var mydate=new Date();
+const dd=new Date();
+console.log(dd.toLocaleString('en-US', { timeZone: 'Asia/Calcutta' }))
+// mydate.setFullYear(2013);
+// mydate.setMonth(02);
+// mydate.setDate(28);
+// mydate.setHours(7);
+// mydate.setMinutes(00);
+
+// console.log(mydate.getDate());
+// console.log(mydate.setDate(06));
+
+var d=new Date();
+d.setMonth(2);
+console.log(d);
+
+
+// var d=new Date();
+// d.setDate(6);
+// console.log(d);
+
+var d=new Date();
+d.getDate();
+console.log(d);
---
+++ module3/advanced-javascript/string/what is string.js
@@ -0,0 +1,13 @@
+// string is a set of character i.e enclosed within "" or '' or in form of string litrals `` backticks
+
+// let str="i am brijesh";
+// let str1='i am brijesh my age is 34 years old';
+// let str2=`i am brijesh 
+// my age is 34 years old`;
+// // console.log(str2);
+// console.log(str2);
+
+
+let str=new String("i am brijesh"); //string as instance
+// console.log(str);
+console.log(typeof(str));
